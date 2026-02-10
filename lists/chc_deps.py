c1c12b93514c09471e71b231cf79ead73fb3ba4f
Commonwealthrocks <204951789+Commonwealthrocks@users.noreply.github.com>
2026-02-10 20:59:43 +0100

    chc_deps.py; update time format and add new library

--- lists/chc_deps.py
+++ lists/chc_deps.py
@@ -1,8 +1,8 @@
 ## chc_deps.py
-## last updated: 20.11.2025 <d/m/y>
+## last updated: 10/02/2026 <d/m/y>
 import pkg_resources
 
-deps = ["numpy", "cryptography", "argon2-cffi", "colorama", "pyside6", "pygame", "reedsolo", "zstandard", "pyzstd", "zxcvbn"]
+deps = ["numpy", "cryptography", "argon2-cffi", "colorama", "pyside6", "pygame", "reedsolo", "zstandard", "pyzstd", "zxcvbn", "imageio"]
 for d in deps:
     try:
         print(d, "==", pkg_resources.get_distribution(d).version)
